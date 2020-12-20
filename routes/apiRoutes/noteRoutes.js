5a3b9640f93f12a28eaa4d80bbd78549360257a4
Craig5117 <craig.bennett5117@gmail.com>
2020-12-19 21:07:41 -0500

    post route is working with validation

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+const { addNewNote, validateNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes)
 });
 
 router.post('/notes', (req, res) => {
-    // req.body.id = notes.length.toString();
-    console.log(req.body)
-    res.json(req.body)
+    req.body.id = notes.length.toString();
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted. Make sure you enter a title and text.')
+    }
+    else {
+        const note = addNewNote(req.body, notes);
+        res.json(req.body);
+    }    
 });
 
 module.exports = router;
