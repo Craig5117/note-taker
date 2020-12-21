08917cd76fcdba8b9a56b6a1a20bad87b08c331e
Craig5117 <craig.bennett5117@gmail.com>
2020-12-21 14:26:21 -0500

    added wildcard

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 
-// refers root to index.html
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
-
 // refers to notes.html page
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
+
+// refers root to index.html and anything else that is not a defined route
+router.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+
 
 module.exports = router;
